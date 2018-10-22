47f5ad969eef037c78fe4a980d56b38546cd77e1
Shanada Cambell <campbellshanada@outlook.com>
2018-10-21 20:46:13 -0500

    Exercise 2

--- maze.js
+++ maze.js
@@ -2,5 +2,22 @@ window.onload = function() {
     let wall = document.getElementById("boundary1");
     wall.addEventListener("mouseover",function(){
         wall.setAttribute("class","boundary youlose");
+    let walls = document.querySelectorAll(".boundary");
+    let touch_wall = false;
+    for(let index = 0; index < walls.length-1; index++){
+        walls[index].addEventListener("mouseover",function() {
+            walls[index].setAttribute("class", "boundary youlose");
+            touch_wall = true;
+        
+        });
+    }
+    let mouse_maze = document.getElementById("maze");
+    mouse_maze.addEventListener("mouseover",function(){
+        if(touch_wall === true){
+            for(let index = 0; index < walls.length-1; index++){
+                walls[index].setAttribute("class","boundary youlose");
+            }
+        }
+    })
     })
 }
